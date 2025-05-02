9292b09961e6a4f2ad325e08573d353da93bb703
Misbah Aiman <misbahaiman652gmail.com>
2025-05-03 00:15:27 +0500

    updated navigation options

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -15,13 +15,15 @@ export default function NavBar() {
   ];
 
   return (
-    <nav className="flex justify-center space-x-6 mt-8 text-white">
+    <nav className="flex justify-center space-x-6 mt-8">
       {navItems.map((item) => (
         <Link
           key={item.href}
           href={item.href}
-          className={`text-sm uppercase tracking-wide hover:text-green-400 ${
-            pathname === item.href ? 'text-green-400 font-semibold' : ''
+          className={`text-sm uppercase tracking-wide px-4 py-2 rounded-md transition-colors ${
+            pathname === item.href 
+              ? 'bg-white text-black font-semibold' 
+              : 'bg-white/10 text-white hover:bg-white hover:text-black'
           }`}
         >
           {item.label}
